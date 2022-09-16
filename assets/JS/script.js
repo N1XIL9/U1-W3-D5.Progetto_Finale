9cc2e43519d8c4123131fa36c78f9ae3d19af82b
Nicola Lerra <github.nicolalerra@gmail.com>
2022-09-16 16:19:08 +0200

    calcolatrice. progetto finale

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,6 +1,13 @@
-function writeToDisplay(string) {
-    document.getElementById('display').value += string
+let previousValue = null
+
+
+function writeToDisplay(s) {
+
+    document.getElementById('display').value += s
 }
+
+
+
 
 function showResult() {
     let result = eval(document.getElementById('display').value)
@@ -9,14 +16,50 @@ function showResult() {
 }
 
 function operazione() {
-    document.getElementById('display').value = eval(document.getElementById('display').value)
+    // document.getElementById('display').value = eval(document.getElementById('display').value)
 }
 
 function cancelDisplay() {
     document.getElementById('display').value = ''
 }
 
-function addVirgola(aggiugoVirgola) {
-    document.getElementById('display').value += aggiugoVirgola
+
+function deleteC() {
+    let input = document.getElementById("display");
+    input.value = input.value.substring(0, input.value.length - 1);
 }
+
+function calculatePiGrego() {
+    let valueDisplay = parseInt(document.getElementById("display").value)  
+
+    document.getElementById("display").value = valueDisplay + Math.PI;
+}
+
+
+
+
+
+
+
+
+
+
+
+function logToConsole(name) {
+    console.log('Ciao ' + name + '!')
+
+}
+
+logToConsole('nicola')
+logToConsole('giuseppe')
+logToConsole('mario')
+logToConsole('luca')
+
+
+function sum(n1, n2) {
+    let result = n1 + n2
+    writeToDisplay(result)
+}
+
+// sum(1, 2)
 
