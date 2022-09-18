2144f7e38d7ef0052d9928b6d567bd7b6f212696
Nicola Lerra <github.nicolalerra@gmail.com>
2022-09-18 14:46:38 +0200

    Calcolatrice HTML CSS JS

--- CALC 1/assets/JS/script.js
+++ CALC 1/assets/JS/script.js
@@ -20,3 +20,27 @@ function addVirgola(aggiugoVirgola) {
     document.getElementById('display').value += aggiugoVirgola
 }
 
+function calculatePiGrego() {
+    let valueDisplay = parseInt(document.getElementById("display").value)
+
+    document.getElementById("display").value = valueDisplay + Math.PI;
+}
+
+function deleteC() {
+    let input = document.getElementById("display");
+    input.value = input.value.substring(0, input.value.length - 1);
+}
+
+function squareRoot (){
+    let val = document.getElementById("display").value  
+    document.getElementById("display").value = Math.sqrt(val);
+}
+
+function maPower (){
+
+   let x = parseInt(document.getElementById("display").value)
+   let y = parseInt(document.getElementById("display").value)
+   let result = Math.pow(x,y)
+   document.getElementById("display").value = result
+   
+}
